27bf4d8b30db1b97452a6bcebc660c41155d059a
Hira Khan <phr.hirakhan@gmail.com>
2024-01-10 17:50:03 +0100

    application containerized

--- my_app/views.py
+++ my_app/views.py
@@ -11,40 +11,49 @@ class SingleRecipeView(views.APIView):
     get single recipe api
     it should have title, author name, methods, ingredients
     """
-
     def get(self, request, pk):
-        recipe = Recipe.objects.get(id=pk)
-        return views.Response(RecipeSerializer(recipe).data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        recipe_serializer = RecipeSerializer(data=request.POST)
-        if recipe_serializer.is_valid():
-            recipe_serializer.save()
-            return views.Response(recipe_serializer.data, status=status.HTTP_200_OK)
-        else:
-            return views.Response(status=status.HTTP_400_BAD_REQUEST)
-
-    def put(self, request, pk):
         try:
             recipe = Recipe.objects.get(id=pk)
+            a=RecipeSerializer(recipe).data
+            return views.Response(a, status=status.HTTP_200_OK)
         except Recipe.DoesNotExist:
             return views.Response(status=status.HTTP_404_NOT_FOUND)
 
-        recipe_serializer = RecipeSerializer(recipe, data=request.POST, partial=True)
 
-        if recipe_serializer.is_valid():
-            recipe_serializer.save()
-            return views.Response(status=status.HTTP_200_OK)
-        else:
-            return views.Response(status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        try:
+            recipe_dict = request.POST
+            recipe_serializer = RecipeSerializer(data=recipe_dict)
+            if recipe_serializer.is_valid():
+                recipe_serializer.save()
+                return views.Response(recipe_serializer.data, status=status.HTTP_200_OK)
+            else:
+                return views.Response(status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as e:
+            return views.Response(status=status.HTTP_409_CONFLICT)
+
+
+    def put(self, request, pk):
+        try:
+            instance = Recipe.objects.get(id=pk)
+            recipe_serializer = RecipeSerializer(instance, data=request.data, partial=True)
+            if recipe_serializer.is_valid():
+                recipe_serializer.save()
+                return views.Response(recipe_serializer.data, status=status.HTTP_200_OK)
+            else:
+                return views.Response(recipe_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Recipe.DoesNotExist:
+            return views.Response(status=status.HTTP_404_NOT_FOUND)
+
 
     def delete(self, request, pk):
         try:
             recipe = Recipe.objects.get(id=pk)
+            recipe.delete()
+            return views.Response(status=status.HTTP_200_OK)
         except Recipe.DoesNotExist:
             return views.Response(status=status.HTTP_404_NOT_FOUND)
-        recipe.delete()
-        return views.Response(status=status.HTTP_200_OK)
 
 
 class RecipesView(views.APIView):
